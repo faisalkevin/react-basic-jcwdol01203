38639531deabe84d302917a9509e06e2870a0295
faisalkevin <faisalkevinl@gmail.com>
2023-11-20 20:32:09 +0700

    first init

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import hero from './components/hero'
+
+function App() {
+
+  return (<div>
+    <h1> Kevin </h1>
+    <Navbar/>
+    <hero/>
+  </div>)
+
+}
+
+export default App
---
+++ vite-project/src/components/Navbar/index.jsx
@@ -0,0 +1,14 @@
+import "./Navbar.css"
+
+function Navbar() {
+    return(<nav className="navbar">
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/"></a>Service</li>
+        </ul>
+    </nav>
+
+    )
+}
+
+export default Navbar
